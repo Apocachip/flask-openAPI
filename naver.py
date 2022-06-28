ab1a1ce70c75ce135ab72d739da84e49953e3e3b
Choi Ji Hun <100457604+Apocachip@users.noreply.github.com>
2022-06-28 10:57:51 +0900

    Update naver.py

--- naver.py
+++ naver.py
@@ -36,19 +36,17 @@ class NaverSearchResource(Resource) :
         # 1. 클라이언트로부터 데이터를 받아온다.
         query = request.args['query']
         display = request.args['display']
-        sort = request.args['date']
+        sort = request.args['sort']
+
+        data = {'query' : query, 'display' : display, 'sort' : sort}
 
         # 2. 네이버 API를 호출한다.
         headers = {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8', 'X-Naver-Client-Id' : Config.NAVER_CLINET_ID, 'X-Naver-Client-Secret' : Config.NAVER_CLINET_SECRET}
 
-        data = {'query' : query, 'display' : display, 'sort' : sort}
 
         res = requests.get(Config.NAVER_NEWS_SEARCH_URL, data, headers= headers)
 
         res = res.json()
 
-        print(res)
-
         # res의 display 와 items만 있으면 된다.
-
         return {'result' : 'success', 'count' : res['display'], 'items' : res['items']}, 200
